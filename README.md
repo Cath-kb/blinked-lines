aea2e5f8c1668340fb698880d8297eb2ba26310b
Catherine Chernysheva <catherine.nikolaev@gmail.com>
2019-04-16 00:25:26 +0300

    create app

---
+++ README.md
@@ -0,0 +1,4 @@
+# PDFfiller JS School 2 entrants task
+## Lines with blinked elements
+
+[Demo](https://cath-kb.github.io/blinked-lines) | [Source code](https://github.com/Cath-kb/blinked-lines)
---
+++ params.js
@@ -0,0 +1,227 @@
+//
+// Generated with https://next.json-generator.com
+//
+// [{
+//   'repeat(3, 15)' : {
+//     background: function () {
+//       return '#'+(Math.random()*(1<<24)|0).toString(16);
+//     },
+//     updateTime: '{{integer(1, 50)*100}}',
+//     elements: [{
+//       'repeat(1, 4)':
+//         {
+//           background: function () {
+//             return '#'+(Math.random()*(1<<24)|0).toString(16);
+//           },
+//           width: '{{integer(0, 5)*5}}'
+//         }
+//     }]
+//   }
+// }]
+
+export const params = {
+  lines: [
+    {
+      "background": "#204856",
+      "updateTime": 2000,
+      "elements": [
+        {
+          "background": "#5384f3",
+          "width": 5
+        }
+      ]
+    },
+    {
+      "background": "#2aa1ca",
+      "updateTime": 3300,
+      "elements": [
+        {
+          "background": "#66c9a5",
+          "width": 15
+        },
+        {
+          "background": "#6003ac",
+          "width": 20
+        },
+        {
+          "background": "#2201b4",
+          "width": 20
+        },
+        {
+          "background": "#ceb3af",
+          "width": 20
+        }
+      ]
+    },
+    {
+      "background": "#4e4858",
+      "updateTime": 2900,
+      "elements": [
+        {
+          "background": "#6e4ae1",
+          "width": 25
+        },
+        {
+          "background": "#3c9376",
+          "width": 5
+        },
+        {
+          "background": "#bd0753",
+          "width": 10
+        },
+        {
+          "background": "#f71ddc",
+          "width": 10
+        }
+      ]
+    },
+    {
+      "background": "#fe96cf",
+      "updateTime": 600,
+      "elements": [
+        {
+          "background": "#885f4a",
+          "width": 5
+        },
+        {
+          "background": "#1a3b0d",
+          "width": 20
+        },
+        {
+          "background": "#e3bac2",
+          "width": 10
+        }
+      ]
+    },
+    {
+      "background": "#ab6b31",
+      "updateTime": 1700,
+      "elements": [
+        {
+          "background": "#7f54b1",
+          "width": 5
+        },
+        {
+          "background": "#8de9c8",
+          "width": 5
+        }
+      ]
+    },
+    {
+      "background": "#299864",
+      "updateTime": 3800,
+      "elements": [
+        {
+          "background": "#7e89da",
+          "width": 10
+        },
+        {
+          "background": "#a4201d",
+          "width": 25
+        },
+        {
+          "background": "#f7019b",
+          "width": 0
+        },
+        {
+          "background": "#fd5fde",
+          "width": 25
+        }
+      ]
+    },
+    {
+      "background": "#1c17f3",
+      "updateTime": 4200,
+      "elements": [
+        {
+          "background": "#593155",
+          "width": 25
+        }
+      ]
+    },
+    {
+      "background": "#f14047",
+      "updateTime": 2300,
+      "elements": [
+        {
+          "background": "#2ad296",
+          "width": 20
+        }
+      ]
+    },
+    {
+      "background": "#f6dc8b",
+      "updateTime": 3400,
+      "elements": [
+        {
+          "background": "#911bbc",
+          "width": 10
+        },
+        {
+          "background": "#cca4a5",
+          "width": 25
+        }
+      ]
+    },
+    {
+      "background": "#a28e09",
+      "updateTime": 3800,
+      "elements": [
+        {
+          "background": "#30c2e6",
+          "width": 25
+        },
+        {
+          "background": "#be5dec",
+          "width": 5
+        },
+        {
+          "background": "#c61340",
+          "width": 10
+        }
+      ]
+    },
+    {
+      "background": "#903f48",
+      "updateTime": 400,
+      "elements": [
+        {
+          "background": "#f372c1",
+          "width": 50
+        }
+      ]
+    },
+    {
+      "background": "#b5ba15",
+      "updateTime": 3000,
+      "elements": [
+        {
+          "background": "#20c990",
+          "width": 25
+        }
+      ]
+    },
+    {
+      "background": "#4e4a7b",
+      "updateTime": 4300,
+      "elements": [
+        {
+          "background": "#bafc2a",
+          "width": 5
+        },
+        {
+          "background": "#0bbd2e",
+          "width": 20
+        },
+        {
+          "background": "#97804b",
+          "width": 5
+        },
+        {
+          "background": "#7b5397",
+          "width": 20
+        }
+      ]
+    }
+  ]
+}
